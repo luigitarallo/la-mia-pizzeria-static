41acbdc6fba4c712096f5df77a66c9c81eebb893
luigitarallo <tarallo.gigi@gmail.com>
2024-05-10 17:10:14 +0200

    add model pizza with new istances, add bootstrap table with foreach

---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,21 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Photo { get; set; }
+        public decimal Price { get; set; }
+
+        public Pizza() { }
+
+        public Pizza(string name, string description, string photo, decimal price)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Photo = photo;
+            this.Price = price;
+        }
+
+    }
+}
